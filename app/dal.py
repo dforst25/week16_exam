55d6db98508d3ec7a05c0d4a9a093b0a87a8c078
David Forst <dforst@g.jct.ac.il>
2026-02-05 10:46:35 +0200

    finished get_employees_by_age_or_seniority

--- app/dal.py
+++ app/dal.py
@@ -16,8 +16,9 @@ def get_engineering_high_salary_employees():
 
 def get_employees_by_age_and_role():
     employees = CNX.get_coll(COLLECTION_NAME)
-    filtered_emp = employees.find({"job_role.title": {"$in": ["Engineer", "Specialist"]}, "age": {"$gte": 30, "$lte": 45}},
-                                  {'_id': 0})
+    filtered_emp = employees.find(
+        {"job_role.title": {"$in": ["Engineer", "Specialist"]}, "age": {"$gte": 30, "$lte": 45}},
+        {'_id': 0})
     return list(filtered_emp)
 
 
@@ -29,3 +30,8 @@ def get_top_seniority_employees_excluding_hr():
     return list(filtered_emp)
 
 
+def get_employees_by_age_or_seniority():
+    employees = CNX.get_coll(COLLECTION_NAME)
+    filtered_emp = employees.find({"$or": [{"years_at_company": {"$lt": 3}}, {"age": {"$gt": 50}}]},
+                                  {'_id': 0, 'employee_id': 1, 'name': 1, 'age': 1, 'years_at_company': 1})
+    return list(filtered_emp)
